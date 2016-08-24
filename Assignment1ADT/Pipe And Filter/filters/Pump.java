e464ea65ea6fe45c9f3749a161ae7bd31c5838e8
Duan Xu Zhou <dxz1020@hotmail.com>
2016-08-25 03:00:39 +0800

    Completed Pipe and Filter with input fixed.

--- Assignment1ADT/Pipe And Filter/filters/Pump.java
+++ Assignment1ADT/Pipe And Filter/filters/Pump.java
@@ -29,7 +29,10 @@ public class Pump extends Filter {
 			}
 			List<String> inputWordlist= getAllWordlist();
 			List<String> ignoreWordlist= getAllIgnoreWordlist();
-			
+
+			ignoreWordFormatting(ignoreWordlist);
+			inputWordFormatting(inputWordlist, ignoreWordlist);
+
 			writeWord(inputWordlist);
 			writeIgnoreWord(ignoreWordlist);
 
@@ -37,15 +40,42 @@ public class Pump extends Filter {
 			System.out.println("error reading input");
 		}
 	}
-	
+
 	private List<String> getAllWordlist() throws IOException{
 		List<String> allLines = Files.readAllLines(Paths.get(System.getProperty("user.dir") + "\\" + linesFileName));
 		return allLines;
 	}
-	
+
 	private List<String> getAllIgnoreWordlist() throws IOException{
 		List<String> allLines = Files.readAllLines(Paths.get(System.getProperty("user.dir") + "\\" + ignoreFileName));
 		return allLines;
 	}
-	
+
+	private void inputWordFormatting(List<String> inputWordlist, List<String> ignoreWordlist){
+		for(int i=0;i<inputWordlist.size();i++){
+			String temp=(inputWordlist.get(i)).toLowerCase();
+			String[] token=temp.split("\\s");
+			String replacement=new String();
+			for(int j=0;j<token.length;j++){
+				if(!(ignoreWordlist.contains(token[j]))){
+					String temp2=(token[j].substring(0, 1)).toUpperCase()+token[j].substring(1);
+					replacement+=temp2;
+					replacement+=" ";
+				}
+				else{
+					replacement+=token[j];
+					replacement+=" ";
+				}
+			}
+			inputWordlist.set(i, replacement);
+		}
+	}
+
+	private void ignoreWordFormatting(List<String> ignoreWordlist){
+		for(int i=0;i<ignoreWordlist.size();i++){
+			String temp=(ignoreWordlist.get(i)).toLowerCase();
+			ignoreWordlist.set(i, temp);
+		}
+	}
+
 }
