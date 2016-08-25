aa4fd53516964d25fe6c8636b5d1f13a2fea5d43
Duan Xu Zhou <dxz1020@hotmail.com>
2016-08-25 12:08:26 +0800

    Integrated with ADT add input check should user enter invalid integer for selection of architecture

--- Assignment1ADT/Pipe And Filter/filters/Sink.java
+++ Assignment1ADT/Pipe And Filter/filters/Sink.java
@@ -1,6 +1,7 @@
 package filters;
 
-import java.io.EOFException;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.List;
 
 public class Sink extends Filter{
@@ -8,9 +9,22 @@ public class Sink extends Filter{
 	@Override
 	public void run(){
 		List<String> output = readWord();
-		System.out.println("Output:");
-		for(String s:output){
-			System.out.println(s);
+		try {
+			writeOutputToFile(output);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
+	}
+	
+	private void writeOutputToFile(List<String> output) throws IOException{
+		PrintWriter pw = new PrintWriter("out.txt");
+		System.out.println("Output:");
+		for (String sentence: output){
+			pw.println(sentence);
+			System.out.println(sentence);
+		}
+		
+		pw.close();
 	}
 }
--- Assignment1ADT/src/Controller.java
+++ Assignment1ADT/src/Controller.java
@@ -1,27 +1,38 @@
 import java.util.Scanner;
+import filters.AlphabeticalSort;
+import filters.CircularShift;
+import filters.Filter;
+import filters.FilterWrapper;
+import filters.IgnoreWordRemoval;
+import filters.Pump;
+import filters.Sink;
 
 public class Controller {
 	public static void main(String[] args){
-		System.out.println("Please select from the following architectures: (Enter their number)");
-		System.out.println("1. Abstract Data Type");
-		System.out.println("2. Pipes and Filters");
-		
-		Scanner sc = new Scanner(System.in);
-		Integer architectureNumber = 0;
-		if (sc.hasNext()){
-			architectureNumber = sc.nextInt();
+		while(true){
+			System.out.println("Please select from the following architectures (Enter their number):");
+			System.out.println("1. Abstract Data Type");
+			System.out.println("2. Pipes and Filters");
+
+			Scanner sc = new Scanner(System.in);
+			Integer architectureNumber = 0;
+			if (sc.hasNextInt()){
+				architectureNumber = sc.nextInt();
+			}
+
+			if (architectureNumber == 1){
+				ADTController adtController = new ADTController();
+				adtController.run();
+			}
+			else if (architectureNumber == 2){
+				//Call pipe and filter
+				Filter[] filters={new Pump(), new CircularShift(), new IgnoreWordRemoval(),new AlphabeticalSort(),new Sink()};			
+				(new FilterWrapper(filters)).run();
+			}
+			else{
+				System.out.println("Invalid Selection, please try again.\n");
+			}
 		}
-		
-		if (architectureNumber == 1){
-			ADTController adtController = new ADTController();
-			adtController.run();
-		}
-		else if (architectureNumber == 2){
-			//Call pipe and filter
-		}
-		else{
-			//Prompt input again
-		}
-		
+
 	}
 }
